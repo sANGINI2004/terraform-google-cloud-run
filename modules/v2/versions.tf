28c48ec552d7a3a95a5e2f361b710b683693b8fe
Sangini Garg <sanginigarg@google.com>
2025-07-30 16:19:47 +0000

    update versions

--- modules/v2/versions.tf
+++ modules/v2/versions.tf
@@ -29,10 +29,10 @@ terraform {
   }
 
   provider_meta "google" {
-    module_name = "blueprints/terraform/terraform-google-cloud-run:v2/v1.0.3"
+    module_name = "blueprints/terraform/terraform-google-cloud-run:v2/v1.0.4"
   }
 
   provider_meta "google-beta" {
-    module_name = "blueprints/terraform/terraform-google-cloud-run:v2/v1.0.3"
+    module_name = "blueprints/terraform/terraform-google-cloud-run:v2/v1.0.4"
   }
 }
